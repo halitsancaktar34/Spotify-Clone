f2ca29e7c12087b5bd6ef06ce4b1ca5a1032257c
halitsancaktar34 <halitsancaktar34@gmail.com>
2023-11-03 04:10:26 +0300

    Project Completed

---
+++ scripts/constants.js
@@ -0,0 +1,8 @@
+export const url = 'https://shazam.p.rapidapi.com/charts/track?locale=tr-TR&pageSize=20&startFrom=0';
+export const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '53bbfd6a45mshba00130ed68b464p12d86djsn5d5b28fac991',
+		'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+	}
+};
---
+++ scripts/ui.js
@@ -0,0 +1,81 @@
+// html'den gelenler
+export const ele = {
+  list: document.querySelector('#list'),
+  playingInfo: document.querySelector('.playing'),
+  searchForm: document.querySelector('#search-form'),
+  title: document.querySelector('.songs #title'),
+};
+
+// arayüz işlemleri
+export const renderCards = (songs) => {
+  // eski şarkıları sil
+  ele.list.innerHTML = '';
+
+  songs.forEach((song) => {
+    // div oluşturma
+    const div = document.createElement('div');
+
+    // kart elemanına ileride js'den erişmek
+    // için bazı verleri ekliyecez
+    // conditional chaining ?.
+    div.dataset.url = song.hub?.actions.pop().uri;
+    div.dataset.title = song.title;
+    div.dataset.photo = song.images.coverart;
+
+    // class verme
+    div.className = 'card';
+
+    // içeriği belirleme
+    div.innerHTML = `
+            <figure>
+              <img
+                src="${song.images?.coverart}"
+              />
+              <div class="play">
+                <i id="play-btn" class="bi bi-play-fill"></i>
+              </div>
+            </figure>
+            <h4>${song.subtitle}</h4>
+            <p>${song.title}</p>
+    `;
+
+    ele.list.appendChild(div);
+  });
+};
+
+// müzik bilgilerini ekrana basar
+export const renderPlayingInfo = (data) => {
+  ele.playingInfo.innerHTML = `
+      <div class="info ">
+        <img
+        class="animate"
+          src="${data.photo}"
+        />
+        <div>
+          <p>Şuan Oynatilıyor</p>
+          <h3>${data.title}</h3>
+        </div> 
+      </div>
+
+      <audio controls autoplay>
+        <source
+          src="${data.url}"
+        />
+      </audio>
+  `;
+};
+
+export const renderLoader = () => {
+  ele.list.innerHTML = `
+ <div class="cssload-container">
+   <ul class="cssload-flex-container">
+      <li>
+         <span class="cssload-loading cssload-one"></span>
+         <span class="cssload-loading cssload-two"></span>
+         <span class="cssload-loading-center"></span>
+      </li>
+   </ul>
+</div>
+
+  `;
+};
